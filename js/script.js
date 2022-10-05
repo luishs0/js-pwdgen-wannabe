b04f76bc55aba449b6b02fa5da1411524bbcad77
luishs0 <luishs.doc@gmail.com>
2022-10-05 14:34:21 +0200

    scaffolding e codice

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let myName = prompt("Your name");
+console.log(myName);
+
+let mySurname = prompt("Your surname");
+console.log(mySurname);
+
+let myFavoriteColor = prompt("Your favorite color");
+console.log(myFavoriteColor);
+
+
+document.write(`Your password is: ${myName}${mySurname}${myFavoriteColor}21`)
